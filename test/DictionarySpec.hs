0838ab192a5b5912bb1eb1b2f7b2622bf6e5264f
Ben Spalding <bnspalding418@gmail.com>
2020-02-12 12:26:52 -0500

    test first and mockDictionary

--- test/DictionarySpec.hs
+++ test/DictionarySpec.hs
@@ -78,7 +78,9 @@ spec = do
           )
           `shouldBe` 1
   describe "methods" $ do
-    describe "first" $ it "has no tests" $ True `shouldBe` True
+    describe "first"
+      $ it "provides first element of a dictionary"
+      $ first mockDict `shouldBe` apple
     describe "last" $ it "has no tests" $ True `shouldBe` True
     describe "size" $ it "has no tests" $ True `shouldBe` True
     describe "next" $ it "has no tests" $ True `shouldBe` True
@@ -100,3 +102,28 @@ mockDef1 = Definition "an evaluation" "n"
 
 mockDef2 :: Definition
 mockDef2 = Definition "to evaluate" "v"
+
+mockDict :: Dictionary
+mockDict =
+  fromList
+    [ cherry,
+      banana,
+      apple,
+      durian,
+      fig
+    ]
+
+apple :: Entry
+apple = makeEntry1 "apple" "" "" ""
+
+banana :: Entry
+banana = makeEntry1 "banana" "" "" ""
+
+cherry :: Entry
+cherry = makeEntry1 "cherry" "" "" ""
+
+durian :: Entry
+durian = makeEntry1 "durian" "" "" ""
+
+fig :: Entry
+fig = makeEntry1 "fig" "" "" ""
