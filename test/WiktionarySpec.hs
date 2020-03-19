72598e7549d7850c9cb84c21901158bf710001d7
Ben Spalding <bnspalding418@gmail.com>
2020-03-19 18:02:37 -0400

    start testing Wiktionary

--- test/WiktionarySpec.hs
+++ test/WiktionarySpec.hs
@@ -3,10 +3,20 @@ module WiktionarySpec
   )
 where
 
+import qualified Data.ByteString.Lazy as B
 import Test.Hspec
 import Wiktionary
 
 spec :: Spec
 spec = do
-  describe "fromJSON" $ do
-    it "creates a Dictionary from a set of JSON entries" $ False
+  describe "readJSONL" $ do
+    it "parses JSONL data into WiktData" $ False
+  describe "readJSONSingle" $ do
+    it "parses a JSON object int a WiktData object" $ False
+  describe "makeDictionary" $ do
+    it "constructs a Dictionary from a set of WiktData objects" $ False
+  describe "wiktDataToEntry" $ do
+    it "transforms a WiktData object into an Entry" $ False
+
+mockSingle :: B.ByteString
+mockSingle = "{\"word\": \"test\", \"pronunciations\": [{\"accent\":[\"RP\"], \"ipa\": [[\"en\", \"/tɛst\"]]}, \"senses\": [{\"glosses\": [\"this is a test with a tag\"], \"tags\": [\"testy\"]}, {\"glosses\": [\"this is a test with no tag\"]}], \"pos\":\"noun\"}"
