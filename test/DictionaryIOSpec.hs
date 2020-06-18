4a3259de9e887499519e12797f8b7ac01a9c0443
Ben Spalding <bnspalding418@gmail.com>
2020-06-18 12:15:19 -0400

    test for DictionaryIO; Fixes #11

---
+++ test/DictionaryIOSpec.hs
@@ -0,0 +1,17 @@
+module DictionaryIOSpec
+  ( spec,
+  )
+where
+
+import DictionaryIO
+import DictionaryTestData
+import Test.Hspec
+
+spec :: Spec
+spec =
+  describe "read-write"
+    $ it "writes a dictionary to file and reads back the same"
+    $ do
+      writeDictionary "test/test.jsonl" caseMockDictC
+      dict <- readDictionary "test/test.jsonl"
+      dict `shouldBe` caseMockDictC
