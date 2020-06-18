75051e7be9482a36e22f5c8b6ced3c5d7b86ff7a
Ben Spalding <bnspalding418@gmail.com>
2020-06-18 11:29:17 -0400

    move dictionary test data to a reusable location

--- test/DictionarySpec.hs
+++ test/DictionarySpec.hs
@@ -7,6 +7,7 @@ where
 
 import qualified Data.Set as Set
 import Dictionary
+import DictionaryTestData
 import Test.Hspec
 
 spec :: Spec
@@ -151,71 +152,3 @@ spec = do
           ((== "n") . pos)
           (fromList [mockEntryMultiDef, mockEntrySingleDef2])
           `shouldBe` fromList [mockEntrySingleDef]
-
-mockEntrySingleDef :: Entry
-mockEntrySingleDef = Entry "test" (Set.singleton mockDef1) []
-
-mockEntrySingleDef2 :: Entry
-mockEntrySingleDef2 = Entry "test" (Set.singleton mockDef2) []
-
-mockEntryMultiDef :: Entry
-mockEntryMultiDef = Entry "test" (Set.fromList [mockDef1, mockDef2]) []
-
-mockDef1 :: Definition
-mockDef1 = Definition "an evaluation" "n" []
-
-mockDef2 :: Definition
-mockDef2 = Definition "to evaluate" "v" []
-
-mockDict :: Dictionary
-mockDict =
-  fromList
-    [ cherry,
-      banana,
-      fig,
-      apple,
-      durian
-    ]
-
-caseMockDictA :: Dictionary
-caseMockDictA =
-  fromList
-    [ apple,
-      bigAcorn,
-      banana
-    ]
-
-caseMockDictB :: Dictionary
-caseMockDictB =
-  fromList
-    [ apple,
-      bigArrow,
-      banana
-    ]
-
-apple :: Entry
-apple = Entry "apple" Set.empty []
-
-banana :: Entry
-banana = Entry "banana" Set.empty []
-
-cherry :: Entry
-cherry = Entry "cherry" Set.empty []
-
-durian :: Entry
-durian = Entry "durian" Set.empty []
-
-fig :: Entry
-fig = Entry "fig" Set.empty []
-
-zebra :: Entry
-zebra = Entry "zebra" (Set.singleton (Definition "a test zebra" "n" ["animal"])) []
-
-trumpet :: Entry
-trumpet = Entry "trumpet" (Set.singleton (Definition "a test trumpet" "n" [])) []
-
-bigAcorn :: Entry
-bigAcorn = Entry "Acorn" Set.empty []
-
-bigArrow :: Entry
-bigArrow = Entry "Arrow" Set.empty []
