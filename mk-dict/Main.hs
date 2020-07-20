5e7ca31fbb2fad2a3eb781f76bcc663154e0cf5f
Ben Spalding <bnspalding418@gmail.com>
2020-07-20 14:59:02 -0400

    generate dictionary from pairs (no definitions yet)

--- mk-dict/Main.hs
+++ mk-dict/Main.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Dictionary
+import DictionaryIO
 import Options.Applicative
 
 main :: IO ()
@@ -14,7 +20,34 @@ main = run =<< execParser opts
         )
 
 run :: Config -> IO ()
-run config = putStrLn ("pairsFile: " <> pairsFile config)
+run config = do
+  putStrLn "NOTE: currently only using the first 500 entries for testing"
+  putStrLn ("generating dictionary from: " <> pairsFile config)
+  putStrLn ("output file is: " <> outFile config)
+  pairs <- fmap pairFromLine . T.lines <$> TIO.readFile (pairsFile config)
+  putStrLn ("number of pairs (unfiltered): " <> show (length pairs))
+  let pairsFiltered = filter (\(ortho, pron) -> (ortho /= "") && (pron /= "")) pairs
+      pairsTEMP = take 500 pairsFiltered -- something manageable for testing
+  putStrLn ("number of pairs: " <> show (length pairsFiltered))
+  pairs' <- case defsFile config of
+    Nothing -> return $ (\(ortho, pron) -> (ortho, [], pron)) <$> pairsTEMP
+    Just f -> addDefs f pairsTEMP
+  let dict = fromList $ uncurry3 makeEntry <$> pairs'
+  putStrLn ("number of entries: " <> show (size dict))
+  writeDictionary (outFile config) dict
+
+pairFromLine :: T.Text -> (T.Text, T.Text)
+pairFromLine l =
+  let (ortho, pron) = T.breakOn "\t" l
+   in (T.strip ortho, T.strip pron)
+
+type PreEntry = (T.Text, [(T.Text, T.Text, [T.Text])], T.Text)
+
+addDefs :: FilePath -> [(T.Text, T.Text)] -> IO [PreEntry]
+addDefs = undefined
+
+uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
+uncurry3 f (x, y, z) = f x y z
 
 data Config
   = Config
